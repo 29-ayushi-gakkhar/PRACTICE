f6ede86c973078cf1ccea4c11226865ad7d58d59
ayushi2912 <ayushigakkhar20017@gmail.com>
2022-08-30 23:54:14 +0530

    adding commands

---
+++ day-3-object-js/string.js
@@ -0,0 +1,16 @@
+//string in javascript
+
+
+const foul = 'ayushi';
+//            012345    - index 
+console.log(foul.length);
+console.log(foul[3]);
+
+
+//using forloop in it 
+
+for(let i=0; i<=5;i++)
+{
+    console.log(foul[i]);
+
+}
