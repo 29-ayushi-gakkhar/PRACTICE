666c67ed9acdfde8f933b8db9b445d971ef472b5
ayushi2912 <ayushigakkhar20017@gmail.com>
2022-09-05 02:27:08 +0530

    commands

--- day9-mongoDB/server.js
+++ day9-mongoDB/server.js
@@ -2,16 +2,16 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const PORT = 8000;
-const MONGO_DB_URL = 'mongodb+srv://user:1234@cluster0.2vaxtuu.mongodb.net/?retryWrites=true&w=majority'
+const MONGO_DB_URL = 'mongodb+srv://user:1234@cluster0.vj9s5df.mongodb.net/?retryWrites=true&w=majority'
 const connectToDB = async () => {
     mongoose.connect(MONGO_DB_URL,
-       err => {
-        if (err) throw err ;
-        console.log('connecting to mongoDB Database')
-       } );
+        err => {
+            if (err) throw err;
+            console.log('connected to MongoDB Database')
+        });
 }
 connectToDB();
 
-app.listen(PORT , function(){
-    console.log('server running on port',PORT);
+app.listen(PORT, function () {
+    console.log('Server running on port', PORT);
 });
