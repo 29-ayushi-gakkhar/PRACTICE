821e0c7a89643758935fb98720e4f45c792eef9b
ayushi2912 <ayushigakkhar20017@gmail.com>
2022-09-08 01:20:37 +0530

    adding commands

---
+++ day10-mongoDB/main.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const app = express();
+
+const connectToDB = require("./DB/index.js");
+const Usermodel = require("./models/user.model.js");
+
+const PORT = 8000;
+
+connectToDB();
+// using this we can send and view json objects
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("It is working");
+});
+
+
+app.post("/adduser", (request, response) => {
+  //console.log(request.body);
+  
+  // destructureing of data
+  // const { names, phone } = request.body;
+  // console.log('Name:' +names)
+  // console.log('Phone no:' +phone)
+  // response.send("please check ");
+
+  const bodyname = request.body.name;
+  const bodyphone = request.body.phone;
+  console.log(bodyname,bodyphone);
+
+//   const userDetails = new Usermodel({
+//   name: bodyname,
+//   phone: bodyphone, 
+//   })
+
+//   console.log(userDetails);
+//   userDetails.save();
+// });
+});
+console.log(Usermodel);
+app.listen(PORT, function () {
+  console.log("Data is on port ", +PORT);
+});
